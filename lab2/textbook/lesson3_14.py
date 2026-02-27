f0bcb5f100f24132b26535040bb188b8a6360c45
User <User@wl5033.surgu.ru>
2026-02-27 11:27:54 +0500

    end textbook exs

---
+++ lab2/textbook/lesson3_14.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+x = [i for i in range(10)]
+y = [i*2 for i in range(10)]
+
+plt.plot(x, y)
+plt.xlabel('Ось X\nНезависимая величина', fontsize=14, fontweight='bold')
+plt.ylabel('Ось Y\nЗависимая величина', fontsize=14, fontweight='bold')
+
+plt.show()
