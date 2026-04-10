bf34cdb843047d273f9fe6e810900b0d0fa5e956
User <User@wl5033.surgu.ru>
2026-04-10 12:31:23 +0500

    api completed

--- lab8/app/main.py
+++ lab8/app/main.py
@@ -3,6 +3,7 @@ from contextlib import asynccontextmanager
 from database.database import engine
 from database.models import Base
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers.nodes import router as nodes_router
 
 @asynccontextmanager
@@ -19,6 +20,20 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 # middleware cors
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost",
+        "http://127.0.0.1",
+        "http://127.0.0.1:5500",
+        "http://localhost:5000",
+        "null"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["*"]
+)
 
 # include routers
 app.include_router(nodes_router)
--- lab8/app/schemas/nodes.py
+++ lab8/app/schemas/nodes.py
@@ -1,5 +1,17 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from datetime import datetime
 
 class CreateSchema(BaseModel):
     name: str
     description: str
+
+class NodeResponseSchema(BaseModel):
+    name: str
+    description: str
+    status: str
+    updated_at: datetime
+
+class UpdateSchema(BaseModel):
+    name: str | None = Field(default=None)
+    description: str | None = Field(default=None)
+    status: str | None = Field(default=None)
